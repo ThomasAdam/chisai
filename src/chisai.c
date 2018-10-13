f298824d74bc1e46126aa1e0e408b3f450864e99
iiPlasma <ZIvy042003@gmail.com>
2018-10-13 17:17:27 -0400

    Sat 13: more work

--- src/chisai.c
+++ src/chisai.c
@@ -1,13 +1,19 @@
 // Chisai - Window Manager
 // Imports
 #include <xcb/xcb.h>
+#include <stdbool.h>
 
 #define MAX(a, b) ((a > b) ? (a) : (b))
+
+// Function Signatures
 
 // Main Event Loop
 int
 main(void)
 {
-    
+    while(true)
+    {
+        
+    }
 }
 
