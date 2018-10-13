a90b5b21f36c71a9236dbdf1df3b4a11f4e15650
iiPlasma <ZIvy042003@gmail.com>
2018-10-13 19:23:51 -0400

    Sat 13: Maikuro with more changes C:

--- src/maikuro.c
+++ src/maikuro.c
@@ -1,12 +1,22 @@
-// Maikuro - Chisai's Client
-// Imports
+/* Maikuro - Chisai's Client */
+/* Imports */
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
-// Helper Function - Exit with error
+
+/*
+ * Function: die
+ * -------------
+ * Returns the message and exists with an error
+ *
+ * fmt: Error message to return
+ * ...: Discard the rest of the arguments
+ *
+ * returns: nothing, just exists the file with the error
+ */
 static void
 die(char *fmt, ...)
 {
@@ -17,34 +27,50 @@ die(char *fmt, ...)
     exit(EXIT_FAILURE);
 }
 
-// Variables
+
+/* Variables */
 int sock_fd;
 const char *sock_path;
 struct sockaddr_un sock_addr;
 
-// Main Client
+/*
+ * Function: main
+ * --------------
+ * Main loop of the client, passes messages to Chisai
+ *
+ * argc: number of arguments
+ * argv: the arguments
+ * 
+ * returns: error if something goes wrong, else messages
+ * just get passed into Chisai
+ */
 int 
 main(int argc, char* argv[])
 {
-    // Make sure there are enough arguments
+    /* Make sure there are enough arguments */
     if (argc < 2) {
         die("%s: not enough arguments", argv[0]);
     }
     
-    // Open socket
+    /* Open socket */
     if ((sock_fd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
 	    die("%s: failed to open socket\n", argv[0]);
     }
 
     sock_addr.sun_family = AF_UNIX;
 
-    // Get socket path
+    /* Get socket path */
     sock_path = getenv("CHISAI_SOCKET");
     
 
     if (sock_path) {
-	strncpy(sock_addr.sun_path, sock_path, sizeof(sock_addr.sun_path));
+        strncpy(sock_addr.sun_path, sock_path, sizeof(sock_addr.sun_path));
     } else {
-	strncpy(sock_addr.sun_path, "/tmp/chisai.sock", sizeof(sock_addr.sun_path));
+        strncpy(sock_addr.sun_path, "/tmp/chisai.sock", sizeof(sock_addr.sun_path));
     }
+
+    /* Connect to Chisai socket */
+    if (connect(sock_fd, (struct sock_addr*)&sock_addr, sizeof(sock_addr)) < 0)  {
+        die("%s: failed to connect to socket\n", argv[0]);
+    }    
 }
